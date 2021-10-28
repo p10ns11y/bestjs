20248074941a607e252e3b47d86a3c8becde6201
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2021-10-28 20:53:56 +0200

    Use jest-haste-map for efficient crawling of files

--- index.mjs
+++ index.mjs
@@ -1,5 +1,21 @@
-import glob from 'glob';
+import JestHasteMap from 'jest-haste-map';
+import { cpus } from 'os';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const testFiles = glob.sync('**/*.test.js');
+// root path of the project (__dirname)
+const root = dirname(fileURLToPath(import.meta.url));
+
+const hasteMap = new JestHasteMap.default({
+  extensions: ['js'],
+  maxWorkers: cpus().length,
+  name: 'best-test-framework',
+  platforms: [],
+  rootDir: root,
+  roots: [root],
+});
+
+const { hasteFS } = await hasteMap.build();
+const testFiles = hasteFS.getAllFiles();
 
 console.log(testFiles);
